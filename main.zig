43b6071914cb999e113c027eaabf58c53ef97cd3
venir.dev <luca@venir.dev>
2023-05-20 00:23:45 +0200

    Woah, an union type
    So that's how you handle errors in Zig..

--- main.zig
+++ main.zig
@@ -1,10 +1,11 @@
 const std = @import("std");
+const AllocationError = error{OutOfMemory};
 
 pub fn main() void {
     const result = function();
-    std.debug.print("Hello, {}!\n", .{result});
+    std.debug.print("Hello, {}!\n", .{result catch 0}); // Hello, 2!
 }
 
-fn function() i32 {
+fn function() AllocationError!u16 {
     return 2;
 }
