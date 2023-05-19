067760650b6bd9237daff8338425595e2fa6b2d5
venir.dev <luca@venir.dev>
2023-05-20 00:53:27 +0200

    Slices are weird...

--- main.zig
+++ main.zig
@@ -5,16 +5,25 @@ pub fn main() void {
     const result = function();
     std.debug.print("Hello, {}!\n", .{result catch 0}); // Hello, 2!
 
-    const anotherResult = fixedLengthVectors();
+    const anotherResult = vectors();
     std.debug.print("Here's a vector, I wonder how it prints out: {s}\n", .{anotherResult});
+
+    const yetAnotherResult = vectors();
+    std.debug.print("Here's a slice, I wonder how it prints out: {s}\n", .{yetAnotherResult});
 }
 
 fn function() AllocationError!u16 {
     return 2;
 }
 
-fn fixedLengthVectors() [5]u8 {
+fn vectors() [5]u8 {
     const b = [5]u8{ 'w', 'o', 'r', 'l', 'd' };
 
     return b;
+}
+
+fn slices() []u8 {
+    const b = [5]u8{ 'w', 'o', 'r', 'l', 'd' };
+
+    return b[0..2];
 }
