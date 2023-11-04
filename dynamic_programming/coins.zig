f2871895192528891d008f413b7d48c9ea49dc56
venir.dev <luca@venir.dev>
2023-11-04 16:14:05 +0100

    some *spice* to the problem

--- dynamic_programming/coins.zig
+++ dynamic_programming/coins.zig
@@ -1,20 +1,20 @@
 const std = @import("std");
 const expect = @import("std").testing.expect;
 
-const available_coins = [_]u32{1,4,5};
+const available_coins = [_]u16{ 1, 4, 5, 8 };
 
 pub fn naiveCoins(input: u32) ?u32 {
-    if(input == 0) {
+    if (input == 0) {
         return 0;
     }
 
     var min: ?u32 = null;
     for (available_coins) |coin| {
-        if(input < coin) {
+        if (input < coin) {
             continue;
         }
         const subproblem = input - coin;
-        
+
         const compute = naiveCoins(subproblem);
         if (compute == null) {
             continue;
@@ -34,7 +34,7 @@ pub fn naiveCoins(input: u32) ?u32 {
     return min.?;
 }
 
-pub fn coins (input: u32) !u32 {
+pub fn coins(input: u32) !u32 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allocator = gpa.allocator();
     var memo = std.AutoHashMap(u32, u32).init(allocator);
@@ -55,23 +55,48 @@ pub fn coins (input: u32) !u32 {
             const computation = sub_computation.? + 1;
 
             const current = memo.get(i);
-            if(current == null) {
+            if (current == null) {
                 try memo.put(i, computation);
                 continue;
             }
 
-            if(computation < current.?) {
+            if (computation < current.?) {
                 try memo.put(i, computation);
             }
         }
         i += 1;
     }
     return memo.get(input).?;
-
 }
 
+pub fn coinWays(input: u16) !u128 {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var allocator = gpa.allocator();
+    var memo = std.AutoHashMap(u16, u128).init(allocator);
+    defer memo.deinit();
+    try memo.put(0, 1);
+    var i: u16 = 1;
+    while (i <= input) {
+        try memo.put(i, 0);
+        for (available_coins) |coin| {
+            if (i < coin) {
+                continue;
+            }
+            const sub = memo.get(i - coin).?;
+            const current = memo.get(i).?;
+            try memo.put(i, sub + current);
+        }
+        i += 1;
+    }
+    return memo.get(input).?;
+}
 
-test "coins" {
-    const result = try coins(13);
-    try expect(result == 3);
+// test "coins" {
+//     const result = try coins(13);
+//     try expect(result == 3);
+// }
+
+test "coins ways" {
+    const result = try coinWays(87);
+    try expect(result == 3306149332861088);
 }
